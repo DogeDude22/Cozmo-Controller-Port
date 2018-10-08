c26d5f9732ef03cb3a8de003b8adbc87635c9e02
DogeDude22 <43528012+DogeDude22@users.noreply.github.com>
2018-10-08 14:28:27 -0700

    Now Cozmo can drive forward

--- Port.py
+++ Port.py
@@ -1,1 +1,7 @@
-import cozm
+import cozmo
+from cozmo.util import degrees, distance_mm, speed_mmps
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
+    
+cozmo.run_program(cozmo_program)
