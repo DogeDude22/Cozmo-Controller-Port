202aa7f25767a524ff1af3a6bda0b37cec359c07
DogeDude22 <43528012+DogeDude22@users.noreply.github.com>
2018-10-14 20:23:43 -0700

    Add files via upload

--- Port.py
+++ Port.py
@@ -1,3 +1,4 @@
+"""Simple example showing how to get gamepad events."""
 
 from __future__ import print_function
 from inputs import get_gamepad
@@ -5,26 +6,41 @@ import cozmo
 from cozmo.util import degrees, distance_mm, speed_mmps
 
 
-
 def main(robot: cozmo.robot.Robot):
- while 1: 
-    events = get_gamepad() 
-    for event in events: 
-        print(event.code,event.state) 
-        if event.code == "ABS_RZ":
-               robot.say_text("I am moving").wait_for_completed()
-               robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
-        if event.code == "ABS_Z": 
-               robot.say_text("I am moving backward").wait_for_completed()
-               robot.drive_straight(distance_mm(-150), speed_mmps(50)).wait_for_completed()
+    while 1: 
+        events = get_gamepad() 
+        for event in events:  
+            print(event.code, event.state)
+            if event.code == "ABS_RZ" and event.state > 150:
+                robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
+            if event.code == "ABS_Z" and event.state > 150:
+               robot.drive_straight(distance_mm(-150), speed_mmps(50)).wait_for_completed() 
+            if event.code == "ABS_HAT0Y" and event.state == -1:
+                robot.move_lift(5)
+            if event.code == "ABS_HAT0Y" and event.state == 1:
+                robot.move_lift(-5)
+            if event.code == "ABS_RY" and event.state > 0:
+                robot.move_head(5)
+            if event.code == "ABS_RY" and event.state < 0:
+                robot.move_head(-5)
+                
+ 
 
-               
-               
 
-            
-
-
-cozmo.run_program(main)
+cozmo.run_program(main, use_viewer=True, force_viewer_on_top=True)
 
 if __name__ == "__main__": 
       main() 
+
+
+ 
+  
+
+
+
+
+
+
+
+
+
